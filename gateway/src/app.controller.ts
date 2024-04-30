450325601cbe2a9b0098c30fd49fea86fd596964
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-30 14:44:55 +0700

    add: MessagePattern

--- gateway/src/app.controller.ts
+++ gateway/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Observable } from 'rxjs';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,20 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('user')
+  getUser(): Observable<string> {
+    return this.appService.getUser();
+  }
+
+  @Get('product')
+  getProduct(): Observable<string> {
+    return this.appService.getProduct();
+  }
+
+  @Get('order')
+  getOrder(): Observable<string> {
+    return this.appService.getOrder();
   }
 }
