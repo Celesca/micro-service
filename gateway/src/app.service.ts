88ad6fbfe16d44d87b1f963f3484587a6ee8556c
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-30 11:38:13 +0700

    add: service

--- gateway/src/app.service.ts
+++ gateway/src/app.service.ts
@@ -1,8 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject('USER_SERVICE') private readonly userClient: ClientProxy,
+    @Inject('PRODUCT_SERVICE') private readonly productClient: ClientProxy,
+    @Inject('ORDER_SERVICE') private readonly orderClient: ClientProxy,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getUser(): Observable<string> {
+    return this.userClient.send({ cmd: 'get/user' }, {});
+  }
+
+  getProduct(): Observable<string> {
+    return this.productClient.send({ cmd: 'get/product' }, {});
+  }
+
+  getOrder(): Observable<string> {
+    return this.orderClient.send({ cmd: 'get/order' }, {});
   }
 }
