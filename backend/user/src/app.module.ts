f6d0428d449ac03d951c057ed8ee7d642934bed9
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-01 23:46:08 +0700

    add: CRUD user

--- backend/user/src/app.module.ts
+++ backend/user/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 @Module({
-  imports: [UserModule],
+  imports: [UsersModule, MongooseModule.forRoot('mongodb://root:example@localhost:27017/userdb?authSource=admin'), UsersModule],
   controllers: [],
   providers: [],
 })
