563ecbc11134e8a46a3e81b08e8dc5019dca35d7
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-02 11:58:04 +0700

    add: users folder in gateway

--- backend/gateway/src/app.module.ts
+++ backend/gateway/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GlobalClientModule } from './client.module';
+import { UsersModule } from './users/users.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [GlobalClientModule],
+  imports: [GlobalClientModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/gateway/src/users/users.controller.ts
+++ backend/gateway/src/users/users.controller.ts
@@ -1,33 +1,25 @@
-import {
-  Controller,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @MessagePattern({ cmd: 'get/users' })
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
-  @MessagePattern({ cmd: 'get/usersById' })
-  findOne(@Payload() id: number) {
+  @Get(':id')
+  findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
-  }
-
-  
-  @MessagePattern({ cmd: 'post/users' })
-  create(@Payload() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
   }
 
   @Patch(':id')
